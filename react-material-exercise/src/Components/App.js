be5c3190e91696d38d3a7a20ca8769962c99f118
Mauricio Gomez <39291659+darkblue94@users.noreply.github.com>
2019-03-15 20:39:21 -0700

    Create App.js

---
+++ react-material-exercise/src/Components/App.js
@@ -0,0 +1,2 @@
+# react-material-exercise
+Building along slide a tutorial I build out the components that allow the user to pull up workouts specifically by a the muscle group property of the exercise object.Material UI was created by google and I enjoy their very minimalist approach that brings more focus to the content.
